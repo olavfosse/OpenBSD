ea2cdda573bc16c31d30c164e618b0355f3adde0
Olav Fosse <fosseolav@gmail.com>
2020-08-10 15:39:10 +0200

    Merge bin/play_songs.sh and bin/songs.sh to bin/songs

--- bin/play_songs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# SYNOPSIS
-#-------------------------------------------------------------------------------
-# Play all OpenBSD release songs
-#
-# See ./songs.sh
-find ~/Music/OpenBSD/ -name "*.ogg" -exec ffplay -nodisp -autoexit {} \;
