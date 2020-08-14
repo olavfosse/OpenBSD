d918f80853809a7f8f4d2e69809dcecf122c561f
Olav Fosse <fosseolav@gmail.com>
2020-08-14 23:18:35 +0200

    Refactor errno to use argv[0] to represent itself

--- src/errno.c
+++ src/errno.c
@@ -14,14 +14,10 @@
 #include <limits.h>
 #include <stdlib.h>
 
-void die();
-
 int main(int argc, char** argv) {
-	if(argc == 1) die();
+	if(argc == 1) {
+		fprintf(stderr, "usage: %s errno\n", argv[0]);
+		exit(1);
+	}
 	printf("%s\n", strerror(atoi(argv[1])));
-}
-
-void die() {
-	fprintf(stderr, "usage: errno errno\n");
-	exit(1);
 }
